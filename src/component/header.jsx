c7ddba5a37ccaded1cc2c3a8939f1d3812c0d4dc
Fasterino <rinofaster89@gmail.com>
2025-10-29 14:44:30 +0700

    first commit

---
+++ src/component/header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Math Matrix</h1>
+      <ul>
+        <li>home</li>
+        <li>about</li>
+        <li>contact</li>
+        <li>start</li>
+      </ul>
+    </header>
+  );
+}
